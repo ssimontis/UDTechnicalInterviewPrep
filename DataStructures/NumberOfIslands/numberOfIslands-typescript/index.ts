5a1e69dd18e412fe7b4e47c5f83b470e6faad7d8
Scott Simontis <ssimontis@gmail.com>
2021-12-05 13:56:29 -0500

    Number of islands problem

---
+++ DataStructures/NumberOfIslands/numberOfIslands-typescript/index.ts
@@ -0,0 +1,14 @@
+// Given a map that represents water (0) or land (1), return the number
+// of islands in the map.
+//
+// Land can connect only to land that is directly above/below it,
+// 
+// [[0, 1],  should return 2
+//  [1, 0]]
+// 
+// [[1, 0],
+// [1, 0]] should return 1
+
+export const numberOfIslands = (map: number[][]): number => {
+  // Implement this!
+}
